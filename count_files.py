62673ff6198cee466d874a7d8fe35b82a9e176e6
om kharate <omkharate65@gmail.com>
2026-02-25 23:55:54 +0530

    Delete count_files.py

--- count_files.py
+++
@@ -1,9 +0,0 @@
-import glob
-import os
-
-ravdess_path = 'C:/MindHaven/archive'  # Updated path
-audio_files = glob.glob(os.path.join(ravdess_path, 'Actor_*/*.wav'))
-print(f"Total audio files: {len(audio_files)}")
-for actor_folder in glob.glob(os.path.join(ravdess_path, 'Actor_*')):
-    actor_files = glob.glob(os.path.join(actor_folder, '*.wav'))
-    print(f"{os.path.basename(actor_folder)}: {len(actor_files)} files")
